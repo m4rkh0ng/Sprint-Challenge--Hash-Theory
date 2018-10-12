0db6194556d799060dcc70829303297e33c27336
Mark Hong <m4rkh0ng@gmail.com>
2018-10-12 13:40:32 -0400

    pass ex2

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,21 @@
 def reconstruct_trip(tickets):
-  pass 
+  ht = {}
+  reconstruct_list = []
+  for ticket in tickets:
+    ht[ticket[0]] = ticket[1]
+  
+  reconstruct_list.append(ht[None])
+  print("None", reconstruct_list)
+
+  for i in range(1, len(ht)-1):
+    try:
+      reconstruct_list.append(ht[reconstruct_list[i-1]])
+    except:
+      reconstruct_list = []
+      print(reconstruct_list)
+      return reconstruct_list
+  print(reconstruct_list)
+  return reconstruct_list
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
