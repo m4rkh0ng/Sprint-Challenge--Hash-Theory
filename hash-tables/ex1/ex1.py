73dae050dd4ed98b5813d3cb56a43ef49d270aca
Mark Hong <m4rkh0ng@gmail.com>
2018-10-12 13:14:12 -0400

    pass ex1

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,5 +1,23 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  ht = {}
+  
+  if len(weights) <= 1:
+    return ()
+
+  if len(weights) == 2:
+    if (weights[0] + weights[1] == limit):
+      return (1,0)
+  
+  for i, weight in enumerate(weights, 0):
+    ht[weight] = i
+
+  for key in ht:
+    try:
+      key_check = ht[limit-key]
+      if(key_check):
+        return (ht[limit-key], ht[key])
+    except KeyError:
+      pass
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
